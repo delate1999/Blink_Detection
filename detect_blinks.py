c7febb82d1f915f6d1aed9ae2e4731eb6b2c1e39
Mateusz Kozlowski <mat.kozlowski99@gmail.com>
2019-03-29 21:48:48 +0100

    Added imports and function calculating eye aspect ratio

---
+++ detect_blinks.py
@@ -0,0 +1,19 @@
+from scipy.spatial import distance as dist
+from imutils.video import FileVideoStream
+from imutils.video import VideoStream
+from imutils import face_utils
+import numpy as np
+import argparse
+import imutils
+import time
+import dlib
+import cv2
+
+def eye_aspect_ratio(eye):
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+    C = dist.euclidean(eye[0], eye[3])
+
+    ear = (A + B)/(2.0 * C)
+
+    return ear
